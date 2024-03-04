c2a5c49e76dcc5a2ecd24b794ab606a0d2bc887b
Ivan Markovski <sagitariusim@gmail.com>
2024-03-04 21:52:14 +0100

    unit tests for API

--- cmd/api/api-handlers.go
+++ cmd/api/api-handlers.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -41,7 +45,54 @@ func (app *application) authenticate(c *gin.Context) {
 }
 
 func (app *application) refresh(c *gin.Context) {
+	err := c.Request.ParseForm()
+	if err != nil {
+		c.Writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	refreshToken := c.Request.Form.Get("refresh_token")
+	claims := &Claims{}
+
+	_, err = jwt.ParseWithClaims(refreshToken, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(app.JWTSecret), nil
+	})
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+
+	if time.Unix(claims.ExpiresAt.Unix(), 0).Sub(time.Now()) > 30*time.Second {
+		c.JSON(http.StatusTooEarly, gin.Error{
+			Err: errors.New("refresh token not required to be renewed"),
+		})
+		return
+	}
+
+	userId, err := strconv.Atoi(claims.Subject)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+
+	user, err := app.DB.GetUser(userId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.Error{
+			Err: errors.New("unknown user"),
+		})
+		return
+	}
+
+	tokenPairs, err := app.generateTokenPairs(user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+	c.SetCookie("__Host-refresh_token", tokenPairs.RefreshToken, int(refreshTokenExpiry.Seconds()),
+		"/", "localhost", true, true)
+
+	c.JSON(http.StatusOK, tokenPairs)
 }
 
 func (app *application) allUsers(c *gin.Context) {
